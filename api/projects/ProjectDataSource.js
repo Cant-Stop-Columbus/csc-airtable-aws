520870bb4783224bc4daafe3fa851a12a5d48a05
mark harris <ma_harris1011@hotmail.com>
2020-04-21 10:57:49 -0400

    filter for active projects

--- api/projects/ProjectDataSource.js
+++ api/projects/ProjectDataSource.js
@@ -4,4 +4,18 @@ export default class ProjectDataSource extends AirtableDataSource {
   constructor() {
     super("Projects", "Title")
   }
+
+  // only return projects for whom Status="Launched"
+  async list(offset = null) {
+    // returns a promise that resolves to the list of promotions
+    let retval = await this.base(this.view)
+      .select({
+        view: "Master",
+        maxRecords: 100,
+        sort: [{ field: "Title", direction: "asc" }],
+        filterByFormula: "AND({Status} = 'Launched', NOT('CSC Internal?' = 'Yes'))" 
+      })
+      .firstPage()
+    return retval
+  }
 }
